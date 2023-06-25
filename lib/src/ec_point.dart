097f46d828402d264ed7891f455ea52be9e9ff51
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-25 17:47:32 +0300

    add comparison operator

--- lib/src/ec_point.dart
+++ lib/src/ec_point.dart
@@ -5,4 +5,14 @@ class ECPoint {
 
   ///Creates instance of a point with BigInt  and y coordinates
   ECPoint(this.x, this.y);
+
+  ///Comparing operator
+  @override
+  operator ==(Object other) {
+    return (other is ECPoint) && (other.x == x) && (other.y == y);
+  }
+
+  ///Hash code override
+  @override
+  int get hashCode => Object.hash(x, y);
 }
