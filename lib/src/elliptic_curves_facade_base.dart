cf2bfe91446b54779f75a824fdac33ff39c6fa70
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-25 17:37:39 +0300

    add isOnCurve

--- lib/src/elliptic_curves_facade_base.dart
+++ lib/src/elliptic_curves_facade_base.dart
@@ -8,4 +8,10 @@ class EllipticCurveFacade {
   EllipticCurveFacade(EllipticCurve? underlyingCurve) {
     curve = underlyingCurve ?? getSecp256k1() as EllipticCurve;
   }
+
+  ///Determines whenever the point is on curve
+  bool isOnCurve(ECPoint point) {
+    return (point.y * point.y) % curve.p ==
+        ((((point.x * point.x) % curve.p) * point.x + curve.a * point.x + curve.b) % curve.p);
+  }
 }
